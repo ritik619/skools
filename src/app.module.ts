269777bb9187a5319f6234c2a4ebc879c10ece2d
Ritik Saini <ritiksaini619@gmail.com>
2025-08-03 21:15:48 +0530

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { OrgnaizationModule } from './orgnaization/orgnaization.module';
 import { UserRoleModule } from './user-role/user-role.module';
 import { UserRelationshipModule } from './user-relationship/user-relationship.module';
 import { UserRoleHierarchyModule } from './user-role-hierarchy/user-role-hierarchy.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 import { DocumentTemplateModule } from './document-template/document-template.module';
 import { SchoolDocumentModule } from './school-document/school-document.module';
 import { RoomModule } from './room/room.module';
@@ -19,7 +19,7 @@ import { CountryModule } from './country/country.module';
 import { OrgnaizationModule } from './orgnaization/orgnaization.module';
 
 @Module({
-  imports: [OrgnaizationModule, CountryModule, CityModule, StateModule, SchoolModule, AcademicYearModule, GradeModule, SectionModule, RoomModule, SchoolDocumentModule, DocumentTemplateModule, UserModule, UserRoleHierarchyModule, UserRelationshipModule, UserRoleModule],
+  imports: [AuthModule,OrgnaizationModule, CountryModule, CityModule, StateModule, SchoolModule, AcademicYearModule, GradeModule, SectionModule, RoomModule, SchoolDocumentModule, DocumentTemplateModule, UserModule, UserRoleHierarchyModule, UserRelationshipModule, UserRoleModule],
   controllers: [AppController],
   providers: [AppService],
 })
